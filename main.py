99bc7b352455b6f88a81380358738208f05d5673
lanzik <aslaheLanark64>
2022-02-13 03:14:46 +0330

    beauty soup:)

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import requests
-import bs4 from BeautifulSoup
+from bs4 import BeautifulSoup
+
+page = requests.get("https://t.me/s/ProxyMTProto")
 
 
+soup = BeautifulSoup(page.text, "html.parser")
 
-
-
+with open('../soup.txt', 'w') as f:
+    f.write(soup.text)
+b = soup.find_all("code")
+for item in b:
+    print(item.text)
+print(b[-1].text)
