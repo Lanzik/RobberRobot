02c36630f8681caf3c63afaca53634e59119ad89
lanzik <aslaheLanark64>
2022-02-16 14:38:36 +0330

    v1.1

--- telegram.py
+++ telegram.py
@@ -1,45 +1,64 @@
-import time, requests
+import time, requests, logging
 from bs4 import BeautifulSoup
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
+
+logging.basicConfig(
+    format='salam log: %(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
 def create_post(link):
     ii = 2
 def check(context: CallbackContext):
     next_v_remove_duplicate = False
 
 def write(update: Update, context: CallbackContext):
-    #print("inja")
-    b = soup.find_all(class_ = "tgme_widget_message_inline_button url_button")
+    print("inja")
+    while(True):
+        page = requests.get("https://t.me/s/ProxyMTProto")
+        soup = BeautifulSoup(page.text, "html.parser")
+        last_link = ''
+        b = soup.find_all(class_ = "tgme_widget_message_inline_button url_button")
+        if(len(b) > 0):
+            break
    # print(b)
-    last_link = b[-1].get('href')
+    print("what? " + str(len(b)))
+    last_link = b[len(b) - 1].get('href')
     is_it_proxy = True
     if(is_it_proxy):
         create_post(last_link)
     
     for item in b:
-        context.bot.send_message(chat_id = -1001674856739, text = item.get('href'))
+        context.bot.send_message(chat_id = -1001674856739, text = item.get('href') + " emza")
         time.sleep(10)
-    last_link = b[-1].get('href')
+    last_link = b[len(b) - 1].get('href')
     while(True):
-        page2 = requests.get("https://t.me/s/ProxyMTProto")
-        soup2 = BeautifulSoup(page2.text, "html.parser")
-        b = soup2.find_all(class_ = "tgme_widget_message_inline_button url_button")
+        while(True):
+            page2 = requests.get("https://t.me/s/ProxyMTProto")
+            soup2 = BeautifulSoup(page2.text, "html.parser")
+            b = soup2.find_all(class_ = "tgme_widget_message_inline_button url_button")
+            if(len(b) > 0):
+                break
         item = -1
         print("last link: " + last_link)
-        print("new soup: " + b[-1].get('href'))
+        print("new soup: " + b[len(b) - 1].get('href'))
         while(True):
             if(b[item].get('href') != last_link):
-                context.bot.send_message(chat_id = -1001674856739, text = b[item].get('href'))
+                context.bot.send_message(chat_id = -1001674856739, text = b[item].get('href') + " emza2")
                 item -= 1
                 time.sleep(10)
                 continue
+            last_link = b[len(b) - 1].get('href')
             break
         time.sleep(20)
     
 def stop(update: Update, context: CallbackContext):
     update.s
 def main() -> None:
-    updater = Updater(token = "5296950371:AAFHA-9KT3yMc-g4z3hAlk2eJk-G0rqdoQM", use_context=True)
+    updater = Updater(token = "5198112327:AAF_iHRawyU8aY58bvaq14ni8QB55Yf-fmU", use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("write", write))
     dispatcher.add_handler(CommandHandler("stop", stop))
@@ -48,7 +67,5 @@ def main() -> None:
     updater.idle()
 
 if __name__ == '__main__':
-    page = requests.get("https://t.me/s/ProxyMTProto")
-    soup = BeautifulSoup(page.text, "html.parser")
-    last_link = ''
+
     main()
