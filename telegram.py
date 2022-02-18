fcfadd258df767b3811feb8198f775183834b0c3
lanzik <aslaheLanark64>
2022-02-18 23:38:31 +0330

    delete comments

--- telegram.py
+++ telegram.py
@@ -31,13 +31,11 @@ def write(update: Update, context: CallbackContext):
         b = soup.find_all(class_ = "tgme_widget_message_inline_button url_button")
         if(len(b) > 0):
             break
-   # print(b)
     print("what? " + str(len(b)))
     last_link = b[len(b) - 1].get('href')
 
     
     for item in b:
-        #context.bot.send_message(chat_id = -1001674856729, text = item.get('href') + " sign")
         server = get_text(item.get('href'), 1)
         port = get_text(item.get('href'), 2)
         secret = get_text(item.get('href'), 3)
@@ -58,7 +56,6 @@ def write(update: Update, context: CallbackContext):
         print("new soup: " + b[item].get('href'))
         while(True):
             if(b[item].get('href') != last_link):
-               # context.bot.send_message(chat_id = -1001674856729, text = b[item].get('href') + " sign2")
                 server = get_text(b[item].get('href'), 1)
                 port = get_text(b[item].get('href'), 2)
                 secret = get_text(b[item].get('href'), 3)
@@ -84,23 +81,8 @@ def main() -> None:
     dispatcher.add_handler(CommandHandler("write", write))
     dispatcher.add_handler(CommandHandler("stop", stop))
     updater.start_polling()
-    #updater.stop
     updater.idle()
 
 if __name__ == '__main__':
 
     main()
-
-
-    #problem!!!!!
-#     Traceback (most recent call last):
-#   File "/home/xlzaxsca/virtualenv/RobberRobotV1/3.9/lib/python3.9/site-packages/telegram/ext/dispatcher.py", line 555, in process_update
-#     handler.handle_update(update, self, check, context)
-#   File "/home/xlzaxsca/virtualenv/RobberRobotV1/3.9/lib/python3.9/site-packages/telegram/ext/handler.py", line 198, in handle_update
-#     return self.callback(update, context)
-#   File "/home/xlzaxsca/RobberRobotV1/public/telegram2.py", line 49, in write
-#     if(b[item].get('href') != last_link):
-# IndexError: list index out of range
-#nabayad payame akhar ro edit kone!!!
-#in bugeshe
-#age dota link ya bishtar to post akhar edit beshe faghat link akhar post mishe(ino badan okesh kon)
